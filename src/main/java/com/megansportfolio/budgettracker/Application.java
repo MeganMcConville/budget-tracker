f600181eb410f71434a89f37800cafb4507715f8
Megan <alksdjf>
2019-03-21 14:07:44 -0600

    PER-5 change package name

--- src/main/java/com/megansportfolio/budgettracker/Application.java
+++ src/main/java/com/megansportfolio/budgettracker/Application.java
@@ -1,4 +1,4 @@
-package com.megansportfolio.budgetTracker;
+package com.megansportfolio.budgettracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
