ee000acd754fc9579cd86338d3437fd8b35cf5cc
Megan <alksdjf>
2019-08-13 14:45:40 -0600

    FF-12 Update tests for budgetItemUpdate funtionality

--- src/test/java/com/megansportfolio/budgettracker/budget/TestBudgetService.java
+++ src/test/java/com/megansportfolio/budgettracker/budget/TestBudgetService.java
@@ -1,5 +1,8 @@
 package com.megansportfolio.budgettracker.budget;
 
+import com.megansportfolio.budgettracker.budgetItem.BudgetItem;
+import com.megansportfolio.budgettracker.budgetItemUpdate.BudgetItemUpdate;
+import com.megansportfolio.budgettracker.budgetItemUpdate.BudgetItemUpdateDao;
 import com.megansportfolio.budgettracker.user.User;
 import com.megansportfolio.budgettracker.user.UserDao;
 import org.junit.Assert;
@@ -25,6 +28,9 @@ public class TestBudgetService {
 
     @Mock
     UserDao userDao;
+
+    @Mock
+    BudgetItemUpdateDao budgetItemUpdateDao;
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
@@ -80,7 +86,7 @@ public class TestBudgetService {
     }
 
     @Test
-    public void testGetBudget(){
+    public void testGetBudgetWithDefaultDate(){
         String loggedInUserEmailAddress = "test@test.com";
         User user = new User();
         Mockito.when(userDao.findOneByUsernameIgnoreCase(loggedInUserEmailAddress)).thenReturn(user);
@@ -93,7 +99,22 @@ public class TestBudgetService {
         user.setId(testUserId);
         budget.setUser(user);
 
-        Budget resultBudget = serviceUnderTest.getBudget(loggedInUserEmailAddress, budgetId);
+        List<BudgetItem> budgetItems = new ArrayList<>();
+        BudgetItem budgetItem1 = new BudgetItem();
+        BudgetItem budgetItem2 = new BudgetItem();
+        long budgetItem1Id = 1;
+        budgetItem1.setId(budgetItem1Id);
+        long budgetItem2Id = 2;
+        budgetItem2.setId(budgetItem2Id);
+        budgetItems.add(budgetItem1);
+        budgetItems.add(budgetItem2);
+        budget.setBudgetItems(budgetItems);
+
+        List<BudgetItemUpdate> budgetItemUpdates = new ArrayList<>();
+        Mockito.when(budgetItemUpdateDao.findAllByBudgetItemId(budgetItem1Id)).thenReturn(budgetItemUpdates);
+        Mockito.when(budgetItemUpdateDao.findAllByBudgetItemId(budgetItem2Id)).thenReturn(budgetItemUpdates);
+
+        Budget resultBudget = serviceUnderTest.getBudget(loggedInUserEmailAddress, budgetId, null, null);
 
         Assert.assertEquals(budget, resultBudget);
     }
@@ -117,7 +138,57 @@ public class TestBudgetService {
         budget.setUser(wrongUser);
         expectedException.expect(RuntimeException.class);
 
-        serviceUnderTest.getBudget(loggedInUserEmailAddress, budgetId);
+        serviceUnderTest.getBudget(loggedInUserEmailAddress, budgetId, null, null);
+    }
+
+    @Test
+    public void testGetBudgetWithSelectedDateAndBudgetItemUpdates(){
+        String loggedInUserEmailAddress = "test@test.com";
+        User user = new User();
+        Mockito.when(userDao.findOneByUsernameIgnoreCase(loggedInUserEmailAddress)).thenReturn(user);
+
+        long budgetId = 1;
+        Budget budget = new Budget();
+        Mockito.when(budgetDao.getOne(budgetId)).thenReturn(budget);
+
+        long testUserId = 1;
+        user.setId(testUserId);
+        budget.setUser(user);
+
+        List<BudgetItem> budgetItems = new ArrayList<>();
+        BudgetItem budgetItem1 = new BudgetItem();
+        BudgetItem budgetItem2 = new BudgetItem();
+        long budgetItem1Id = 1;
+        budgetItem1.setId(budgetItem1Id);
+        long budgetItem2Id = 2;
+        budgetItem2.setId(budgetItem2Id);
+        budgetItems.add(budgetItem1);
+        budgetItems.add(budgetItem2);
+        budget.setBudgetItems(budgetItems);
+
+        String testName = "Test";
+        List<BudgetItemUpdate> budgetItemUpdates1 = new ArrayList<>();
+        BudgetItemUpdate budgetItemUpdate1 = new BudgetItemUpdate();
+        budgetItemUpdate1.setMonth(12);
+        budgetItemUpdate1.setYear(2000);
+        budgetItemUpdate1.setName(testName);
+        budgetItemUpdates1.add(budgetItemUpdate1);
+        List<BudgetItemUpdate> budgetItemUpdates2 = new ArrayList<>();
+        BudgetItemUpdate budgetItemUpdate2 = new BudgetItemUpdate();
+        budgetItemUpdate2.setMonth(12);
+        budgetItemUpdate2.setYear(1999);
+        budgetItemUpdate2.setMonthSpecific(false);
+        budgetItemUpdate2.setName(testName);
+        budgetItemUpdates2.add(budgetItemUpdate2);
+        Mockito.when(budgetItemUpdateDao.findAllByBudgetItemId(budgetItem1Id)).thenReturn(budgetItemUpdates1);
+        Mockito.when(budgetItemUpdateDao.findAllByBudgetItemId(budgetItem2Id)).thenReturn(budgetItemUpdates2);
+
+        Budget resultBudget = serviceUnderTest.getBudget(loggedInUserEmailAddress, budgetId, 1, 2000);
+
+        Assert.assertEquals(budget, resultBudget);
+        Assert.assertEquals(testName, budgetItem2.getName());
+        Assert.assertEquals(null, budgetItem1.getName());
+
     }
 
     @Test
@@ -203,5 +274,27 @@ public class TestBudgetService {
         serviceUnderTest.renameBudget(parameterBudget, loggedInUserEmailAddress);
 
     }
+
+    @Test
+    public void testGetDisplayYear(){
+        Integer parameterYear = 1800;
+
+        int resultYear = serviceUnderTest.getDisplayYear(parameterYear);
+
+        Assert.assertEquals(parameterYear.intValue(), resultYear);
+    }
+
+    @Test
+    public void testGetDisplayMonth(){
+        Integer parameterMonth = 5;
+        Month displayMonth = Month.valueOfMonthNumber(parameterMonth);
+        String monthLabel = "May";
+
+        Month resultMonth = serviceUnderTest.getDisplayMonth(parameterMonth);
+
+        Assert.assertEquals(displayMonth, resultMonth);
+        Assert.assertEquals(monthLabel, resultMonth.getNameLabel());
+    }
+
 
 }
