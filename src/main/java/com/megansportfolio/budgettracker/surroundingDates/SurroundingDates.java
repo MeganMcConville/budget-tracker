52ae694f22df9aa1a6dad35c74bc3dbccdc77e22
Megan <alksdjf>
2020-01-08 15:39:44 -0700

    FF-29 User can navigate between months in increments of one both forward and backward from the budget page without going into the date selection modal

---
+++ src/main/java/com/megansportfolio/budgettracker/surroundingDates/SurroundingDates.java
@@ -0,0 +1,44 @@
+package com.megansportfolio.budgettracker.surroundingDates;
+
+public class SurroundingDates {
+
+    private Integer previousMonth;
+
+    private Integer previousYear;
+
+    private Integer nextMonth;
+
+    private Integer nextYear;
+
+    public Integer getPreviousMonth() {
+        return previousMonth;
+    }
+
+    public void setPreviousMonth(Integer previousMonth) {
+        this.previousMonth = previousMonth;
+    }
+
+    public Integer getPreviousYear() {
+        return previousYear;
+    }
+
+    public void setPreviousYear(Integer previousYear) {
+        this.previousYear = previousYear;
+    }
+
+    public Integer getNextMonth() {
+        return nextMonth;
+    }
+
+    public void setNextMonth(Integer nextMonth) {
+        this.nextMonth = nextMonth;
+    }
+
+    public Integer getNextYear() {
+        return nextYear;
+    }
+
+    public void setNextYear(Integer nextYear) {
+        this.nextYear = nextYear;
+    }
+}
---
+++ src/main/java/com/megansportfolio/budgettracker/surroundingDates/SurroundingDatesService.java
@@ -0,0 +1,36 @@
+package com.megansportfolio.budgettracker.surroundingDates;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+
+@Service
+public class SurroundingDatesService {
+
+    public SurroundingDates setSurroundingDates(Integer currentMonth, Integer currentYear){
+
+        SurroundingDates surroundingDates = new SurroundingDates();
+        if(currentMonth == null || currentYear == null){
+            Calendar cal = Calendar.getInstance();
+            currentMonth = (cal.get(Calendar.MONTH)) + 1;
+            currentYear = (cal.get(Calendar.YEAR));
+        }
+
+        surroundingDates.setPreviousMonth(currentMonth - 1);
+        surroundingDates.setPreviousYear(currentYear);
+        surroundingDates.setNextMonth(currentMonth + 1);
+        surroundingDates.setNextYear(currentYear);
+
+        if(currentMonth == 12){
+            surroundingDates.setNextMonth(1);
+            surroundingDates.setNextYear(currentYear + 1);
+        }
+        if(currentMonth == 1){
+            surroundingDates.setPreviousMonth(12);
+            surroundingDates.setPreviousYear(currentYear - 1);
+        }
+
+        return surroundingDates;
+    }
+
+}
